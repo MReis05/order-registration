0b3159e162fb526129e91d25d62cbf0ede0d38d2
Matheus Reis <matheusreiscrs@gmail.com>
2025-05-28 20:59:16 -0300

    IfoodListController

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,61 @@
+package gui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.function.Consumer;
+
+import application.Main;
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+public class MainViewController implements Initializable {
+
+	@FXML
+	private MenuItem menuItemIfood;
+	
+	@FXML
+	private MenuItem menuItemPV;
+	
+	@FXML
+	public void onMenuItemIfoodAction() {
+		loadView("/gui/IfoodListView.fxml", x -> {});
+	}
+	
+	@FXML
+	public void onMenuItemPVAction() {
+		System.out.println("onMenuItemPVAction");
+	}
+	
+	public synchronized <T> void loadView(String absoluteView, Consumer<T> consumer) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteView));
+			VBox newVBox = loader.load();
+
+			Scene mainScene = Main.getMainScene();
+			VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+			Node mainMenu = mainVbox.getChildren().get(0);
+			mainVbox.getChildren().clear();
+			mainVbox.getChildren().add(mainMenu);
+			mainVbox.getChildren().addAll(newVBox.getChildren());
+
+			T controller = loader.getController();
+			consumer.accept(controller);
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error in Loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+	}
+}
---
+++ src/model/entities/Ifood.java
@@ -0,0 +1,91 @@
+package model.entities;
+
+public class Ifood extends Order {
+
+	private Double forIfood;
+	private Double tax;
+	private Integer serviceTax;
+
+	private PaymentMethod payment = new PaymentMethod();
+
+	public Ifood(Order order) {
+		super(order.getId(), order.getOrderValue(), order.getDeliveryValue());
+		taxForIfood(order.getOrderValue());
+	}
+
+	public Ifood(Order order, String payment) {
+		super(order.getId(), order.getOrderValue(), order.getDeliveryValue());
+		taxForStore(order.getOrderValue(), payment);
+	}
+
+	public Ifood(Order order, double cutValue, String payment) {
+		super(order.getId(), order.getOrderValue(), order.getDeliveryValue());
+		cutPayments(order.getOrderValue(), cutValue, payment);
+	}
+
+	public Double getForIfood() {
+		return forIfood;
+	}
+
+	public void setForIfood(Double forIfood) {
+		this.forIfood = forIfood;
+	}
+	
+	public void setTax (Double tax) {
+		this.tax = tax;
+	}
+
+	public Double getTax() {
+		return tax;
+	}
+
+	public Integer getServiceTax() {
+		return serviceTax;
+	}
+	
+	public void setServiceTax(Integer serviceTax) {
+		this.serviceTax = serviceTax;
+	}
+
+	public void taxForIfood(double value) {
+		setForIfood(value);
+		serviceTax = serviceTax(value);
+		tax = value * 0.1679;
+	}
+
+	public void taxForStore(double value, String paymentMethod) {
+		payment.setPaymentMethod(paymentMethod, value);
+		serviceTax = serviceTax(value);
+		tax = value * 0.12;
+	}
+
+	public int serviceTax(double value) {
+		int taxOfService = 0;
+		if (value <= 24.99) {
+			taxOfService = 1;
+		}
+		return taxOfService;
+	}
+
+	public void cutPayments(double order, double cutValue, String paymentMethod) {
+		if (order - cutValue > 0) {
+			payment.setPaymentMethod(paymentMethod, order - cutValue);
+		}
+		else {
+			payment.setPaymentMethod(paymentMethod, cutValue - order);
+		}
+		
+		forIfood = cutValue;
+		tax = (order + cutValue) * 0.12;
+		
+	}
+
+	public PaymentMethod getPayment() {
+		return payment;
+	}
+
+	@Override
+	public String toString() {
+		return "Ifood [forIfood=" + forIfood + ", tax=" + tax + ", serviceTax=" + serviceTax;
+	}
+}
